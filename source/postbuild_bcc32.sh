8f59db234bf0a4a78e50f05635e98a79418cbd1e
TotteKarlsson <tottek@gmail.com>
2018-03-24 11:21:06 -0700

    Added postbuild script

--- source/postbuild_bcc32.sh
+++ source/postbuild_bcc32.sh
@@ -3,82 +3,33 @@ echo "Hello Bash"
 
 echo "Arg1: "$1
 echo "Arg2: "$2
-echo "Arg3: "$3
-echo "Arg4: "$4
 
-OUTPUTDIR=`cygpath $1`
-APP_RELEASES=`cygpath $2`
-OUTPUTFILENAME=$3
-BDSCOMMONDIR=$4
+BDSCOMMONDIR=$1
+AT_ROOT=`cygpath $2`
+BIN_FOLDER=`cygpath $BDSCOMMONDIR`/bin
 
-echo "OUTPUTDIR: "$OUTPUTDIR
-echo "AppReleases: "$APP_RELEASES
-echo "Outputfilename: "$OUTPUTFILENAME
-echo "BDSCOMMONDIR: "$BDSCOMMONDIR
+echo "Bin Folder: "$BIN_FOLDER
+echo "Copy .dll files required for runtime"
 
-#Extract version
-VERSION=`cat ../VERSION.txt | tr -d '\r'`
-APPNAME=ArrayCam
-echo "Copy stuff"
-mkdir $APP_RELEASES/$APPNAME
+TP=$AT_ROOT"/thirdparty"
+#cp $TP/Thorlabs/*.dll                          $BIN_FOLDER
 
-dest=$APP_RELEASES/$APPNAME
+#Thorlabs
+for f in $TP/Thorlabs/*.dll_0; do 
+    cp "$f" $BIN_FOLDER/"$(basename "$f" .dll_0).dll"
+done
 
-cp ./../*.txt $dest
-cp $OUTPUTDIR/$OUTPUTFILENAME $dest
+#Navitar
+for f in $TP/navitar/*.dll_redist; do 
+    cp "$f" $BIN_FOLDER/"$(basename "$f" .dll_redist).dll"
+done
 
-#DS Binaries
-cp $OUTPUTDIR/mtkCommon.dll                    $dest
-cp $OUTPUTDIR/mtkMath.dll                      $dest
-cp $OUTPUTDIR/mtkIPC.dll                       $dest
-cp $OUTPUTDIR/atCore.dll                       $dest
-cp $OUTPUTDIR/atDatabase.dll                   $dest
-cp $OUTPUTDIR/atResources.dll                  $dest
-cp $OUTPUTDIR/atBarcodeReader.dll              $dest
-cp $OUTPUTDIR/atSSI.dll                        $dest
-cp $OUTPUTDIR/atUC7API.dll                        $dest
-cp $OUTPUTDIR/atArduino.dll                        $dest
-cp $OUTPUTDIR/atSerialAPI.dll                        $dest
+#uc480 Camera..
+for f in $TP/uc480/*.dll_0; do 
+    cp "$f" $BIN_FOLDER/"$(basename "$f" .dll_0).dll"
+done
 
- cp $BDSCOMMONDIR/BPL/atVCLCoreBCC32D.bpl             $dest    
- cp $BDSCOMMONDIR/BPL/DuneComponentsBCC32D.bpl      $dest
- cp $BDSCOMMONDIR/BPL/DuneFormsBCC32D.bpl           $dest
- cp $BDSCOMMONDIR/BPL/VCLCommonBCC32D.bpl           $dest
- cp $BDSCOMMONDIR/BPL/ArrayCamPackageBCC32D.bpl           $dest
-
-#Borland Packages and dll's
-CG_REDIST="/cygdrive/c/CodeGear/Tokyo/bin"
-
-cp $CG_REDIST/borlndmm.dll                      $dest
-cp $CG_REDIST/cc32250mt.dll                     $dest
-
-suffix="250.bpl"
-cp $CG_REDIST/bindcomp$suffix                   $dest
-cp $CG_REDIST/bindcompvcl$suffix                $dest
-cp $CG_REDIST/bindengine$suffix                 $dest
-cp $CG_REDIST/dbexpress$suffix                  $dest
-cp $CG_REDIST/dbrtl$suffix                      $dest
-cp $CG_REDIST/dbxcds$suffix                     $dest
-cp $CG_REDIST/DbxCommonDriver$suffix		    $dest
-cp $CG_REDIST/DbxDevartPostgreSQLDriver$suffix	$dest
-cp $CG_REDIST/DBXMySQLDriver$suffix		        $dest
-cp $CG_REDIST/dsnap$suffix                      $dest
-cp $CG_REDIST/rtl$suffix                        $dest
-cp $CG_REDIST/vcl$suffix                        $dest
-cp $CG_REDIST/vclactnband$suffix                $dest
-cp $CG_REDIST/vcldb$suffix                      $dest
-cp $CG_REDIST/vclimg$suffix                     $dest
-cp $CG_REDIST/vclx$suffix                       $dest
-cp $CG_REDIST/Tee9$suffix                       $dest
-
-miscRedist="/cygdrive/p/redist"
-cp $miscRedist/dbexppgsql40.dll                $dest
-#cp $miscRedist/tsclib.dll                      $dest
-cp $miscRedist/navusbapi.dll                    $dest
-cp $miscRedist/uc480.dll                        $dest
-cp $miscRedist/uc480_tools.dll                  $dest
-
-#for local bin folder
-#cp $miscRedist/navusbapi.dll                   $OUTPUTDIR
-cp $miscRedist/uc480.dll                        $OUTPUTDIR
-cp $miscRedist/uc480_tools.dll                  $OUTPUTDIR
+#TSC
+for f in $TP/*.dll_0; do 
+    cp "$f" $BIN_FOLDER/"$(basename "$f" .dll_0).dll"
+done
