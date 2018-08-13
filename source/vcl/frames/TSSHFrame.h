c4279bebd8f0aeb2e3aed23e352833d5cd80db04
Totte Karlsson <tottek@gmail.com>
2018-08-13 11:43:24 -0700

    Minor

--- source/vcl/frames/TSSHFrame.h
+++ source/vcl/frames/TSSHFrame.h
@@ -16,37 +16,34 @@
 
 class PACKAGE TSSHFrame : public TFrame
 {
-__published:	// IDE-managed Components
-	TGroupBox *GroupBox1;
-	TButton *ConnectBtn;
-	TScFileStorage *ScFileStorage;
-	TScSSHShell *ScSSHShell1;
-	TScSSHChannel *ScSSHChannel;
-	TScSSHClient *ScSSHClient;
-	TSTDStringLabeledEdit *edSSHHost;
-	TIntegerLabeledEdit *seSSHPort;
-	TSTDStringLabeledEdit *edSSHUserName;
-	TSTDStringLabeledEdit *edSSHPassword;
-	void __fastcall ScSSHClientAfterConnect(TObject *Sender);
-	void __fastcall ScSSHClientServerKeyValidate(TObject *Sender, TScKey *NewServerKey,
-          bool &Accept);
-	void __fastcall ScSSHClientAfterDisconnect(TObject *Sender);
-	void __fastcall ScSSHClientBeforeConnect(TObject *Sender);
-	void __fastcall ConnectBtnClick(TObject *Sender);
-	void __fastcall ScSSHShell1AsyncError(TObject *Sender, Exception *E);
-private:	// User declarations
+    __published:	// IDE-managed Components
+        TGroupBox *GroupBox1;
+        TButton *ConnectBtn;
+        TScFileStorage *ScFileStorage;
+        TScSSHShell *ScSSHShell1;
+        TScSSHChannel *ScSSHChannel;
+        TScSSHClient *ScSSHClient;
+        TSTDStringLabeledEdit *edSSHHost;
+        TIntegerLabeledEdit *seSSHPort;
+        TSTDStringLabeledEdit *edSSHUserName;
+        TSTDStringLabeledEdit *edSSHPassword;
+        void __fastcall ScSSHClientAfterConnect(TObject *Sender);
+        void __fastcall ScSSHClientServerKeyValidate(TObject *Sender, TScKey *NewServerKey, bool &Accept);
+        void __fastcall ScSSHClientAfterDisconnect(TObject *Sender);
+        void __fastcall ScSSHClientBeforeConnect(TObject *Sender);
+        void __fastcall ConnectBtnClick(TObject *Sender);
+        void __fastcall ScSSHShell1AsyncError(TObject *Sender, Exception *E);
 
-        bool 				FRandomized;
-        void 				SaveState();
-        void 				LoadState();
-        String 				KeyPath();
+    private:
+            bool 				FRandomized;
+            void 				SaveState();
+            void 				LoadState();
+            String 				KeyPath();
 
-
-public:		// User declarations
-				__fastcall  TSSHFrame(TComponent* Owner);
-        bool				isConnected();
-        void				disconnect();
-
+    public:
+                    __fastcall  TSSHFrame(TComponent* Owner);
+            bool				isConnected();
+            void				disconnect();
 };
 
 extern PACKAGE TSSHFrame *SSHFrame;
